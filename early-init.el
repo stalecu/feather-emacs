f6a72373defb79d9e5363c420b3807131f44b63c
Alecu Ștefan-Iulian <uneven-shiver@protonmail.com>
2024-03-15 21:03:30 +0200

    Add copyright to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,5 +1,34 @@
 ;;; early-init.el --- Early Init File -*- lexical-binding: t -*-
 
+;; Copyright (c) 2024 Alecu Ștefan-Iulian
+
+;; Author: Alecu Ștefan-Iulian <uneven-shiver@protonmail.com>
+;; URL: https://github.com/stalecu/feather-emacs
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is the early init file for Feather Emacs. This is the file
+;; that gets run before init.el, and as such this file contains some
+;; optimizations, mainly related to GC, startup speed and UI, as well
+;; as disabling package.el at startup.
+
+;;; Code:
+
 ;; We'll let Elpaca take care of this later on, so we don't want
 ;; interference from use-package.el.
 (setq package-enable-at-startup nil)
